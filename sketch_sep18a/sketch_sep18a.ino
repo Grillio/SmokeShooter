ac90571b1b04dd53b14fce957bb3ee02f9153103
Grillio <bggriller@gmail.com>
2023-09-27 01:23:39 -0400

    Fixed arduino code

--- sketch_sep18a/sketch_sep18a.ino
+++ sketch_sep18a/sketch_sep18a.ino
@@ -6,27 +6,33 @@ AccelStepper shooter(1, 6, 7);
 
 void setup() {
     Serial.begin(4800);
+
     horizontal.setAcceleration(100);
-    horizontal.setMaxSpeed(300);
+    horizontal.setMaxSpeed(200);
 
     vertical.setMaxSpeed(200);
     vertical.setAcceleration(100);
 
     shooter.setAcceleration(100);
     shooter.setMaxSpeed(200);
+    delay(200);
 }
 
+//int horizontalPos = 0;
+
 void loop() {
-  
+
   while(Serial.available()){
       const char Identifier = (char) Serial.read();
       switch(Identifier){
         case ('H'):
+          //Serial.println("Message Received");
         //wait until there are 2 bytes in the serial before executing reading functions
           while(Serial.available() < 1){
 
           }
-          horizontal.move((long) Serial.read());
+          //Serial.println("Executing command");
+          horizontal.moveTo((long) Serial.read());
           break;
         case ('V'):
           while(Serial.available() < 1){
@@ -38,13 +44,9 @@ void loop() {
           while(Serial.available() < 1){
 
           }
-          digitalWrite(10, HIGH);
-          shooter.setCurrentPosition(360);
-          digitalWrite(10, LOW);
           break;
+        
       }
   }
-
-
-
+  horizontal.run();
 }
